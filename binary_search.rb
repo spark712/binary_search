fcd5b655cd135c6222d5e2d7ce2842320efca6e0
spark712 <69505188+spark712@users.noreply.github.com>
2021-03-30 18:29:19 +0530

    Add files via upload

---
+++ binary_search.rb
@@ -0,0 +1,16 @@
+def binary_search(target, list, idx = (0...list.length).to_a)
+  position = (list.length / 2).floor
+  mid = list[position]
+  indx = idx[position]
+  return "The element is at position #{indx}." if mid == target
+  return 'No data found.' if position == 0
+  if target < mid
+    binary_search(target, list[0...position], idx[0...position])
+  else
+    binary_search(target, list[position..-1], idx[position..-1])
+  end
+  
+end
+
+puts binary_search(9, [1,2,3,4,5,6,7,8,10])
+puts binary_search(9, [1,2,3,4,5,6,7,8,9,10])
